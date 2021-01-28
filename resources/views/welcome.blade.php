b5c67307a01ed6db8883e66bd6048cd1017ca639
Aninditaroy <anindita.roy.mu@gmail.com>
2021-01-28 22:42:02 +0600

    contact list code added

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12 mt-2">  
+            <h4 style="color:#2196F3; margin-bottom: 20px;">Get Started absolutely free!</h4>
+            <caption>Hello, Everyone! Login or Register here to your accesss.</caption>
+            <img src="https://images.pexels.com/photos/33999/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" class="img-fluid">
+       </div>
+    </div>
+</div>
+@endsection
